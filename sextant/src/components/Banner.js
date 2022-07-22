42946af9baedb869b53d1dbbc33e99118d2bcd20
LoopFruits <Kensworldc@gmail.com>
2022-07-22 15:09:01 -0400

    added banner

---
+++ sextant/src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Banner(){
+    return (
+        <h1 className= "text-3xl font-bold underline"> 
+        Cisco 
+        </h1>
+    )
+}
+
+
+export default Banner;
